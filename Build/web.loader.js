fb3a68515bbbde4bd0872da4341989b1bc6f47e5
Demir Danış <demirdanis@gmail.com>
2026-04-15 21:10:40 +0300

    deploy

--- Build/web.loader.js
+++ Build/web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58318267,
+    wasmFileSize: 58318205,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
